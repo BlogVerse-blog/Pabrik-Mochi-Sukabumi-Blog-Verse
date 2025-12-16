1f10d553a114d467676ad71df28564f88909308e
Blog Verse <blogverse25@gmail.com>
2025-12-16 15:33:56 +0700

    Update link in app.js to point to new URL

--- Pabrik Moci Sukabumi/js/app.js
+++ Pabrik Moci Sukabumi/js/app.js
@@ -54,7 +54,7 @@ const renderArticle = () => {
             </div>
             
             <div class="article-actions">
-                <a href="./../../Main/index.html" class="action-btn">
+                <a href="https://blog-verse-tawny-ten.vercel.app/" class="action-btn">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
                     </svg>
